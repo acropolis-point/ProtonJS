dc16b6fc733667d372063d3ce8fa75be60255207
l-7777777 <Lysander.mealy@gmail.com>
2020-06-25 15:27:34 -0400

    Commit

--- proton.py
+++ proton.py
@@ -1,39 +1,24 @@
 import os
 import sys
-import json
 
-from gi.repository import GLib
 from gi.repository import Gtk, Gdk
 from gi.repository import WebKit2 as WebKit
 
 webview = WebKit.WebView()
-path = sys.argv[5]
+path = "start.html"
 path = os.path.realpath(path)
 webview.load_uri("file://" + path)
 
-scrolledWindow = Gtk.ScrolledWindow()
-scrolledWindow.add(webview)
-
 window = Gtk.Window()
-
-if (sys.argv[3] == "false"):
-    window.set_decorated(False)
-
-window.set_default_size(int(sys.argv[1]), int(sys.argv[2]))
-window.set_title(sys.argv[4])
-window.set_icon_from_file(sys.argv[6])
-window.add(scrolledWindow)
+window.set_decorated(False)
+window.set_default_size(1, 1)
+window.add(webview)
 window.show_all()
 
 def quit(args):
     Gtk.main_quit()
-window.connect("destroy", quit)
+    os.system("killall flask")
 
-def checkClose():
-    theFile = open("output.json", "r")
-    theFileParsed = json.load(theFile)
-    if (theFileParsed['close'] == sys.argv[4]):
-        Gtk.main_quit()
-GLib.timeout_add(100, checkClose)
+window.connect("destroy", quit)
 
 Gtk.main()
